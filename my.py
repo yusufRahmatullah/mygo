26199d05a807a68b9dc9d59f5cad56edc5992925
Yusuf Rahmatullah <yusufr.main@gmail.com>
2020-09-04 22:51:25 +0700

    implement kube command

--- my.py
+++ my.py
@@ -9,6 +9,8 @@ KNOWN_FILES = 'known_gs.txt'
 GIT_ADDED = 'Git-added'
 NOT_ADDED = 'Not git-added'
 UNTRACKED = 'Untracked'
+# Known kubectl commands
+KB_COMMANDS = ['pods', 'hpa', 'deployment', 'exec', 'edit']
 
 
 def _parse_gs(subparsers):
@@ -31,14 +33,11 @@ def _parse_gs(subparsers):
     )
 
 
-def _parse_gen(subparsers):
-    parser = subparsers.add_parser(
-        'gen',
-        help='Generate 64-char password or pin'
-    )
+def _parse_kb(subparsers):
+    parser = subparsers.add_parser('kb', help='Simplified kubectl')
     parser.add_argument(
-        '-s', '--simple', action='store_true',
-        help='Simple mode'
+        'keywords', nargs='+',
+        help=f'kubectl command arguments {{{" ".join(KB_COMMANDS)}}}'
     )
 
 
@@ -50,7 +49,7 @@ def parse_args():
         subparsers = parser.add_subparsers(dest='cmd')
         subparsers.required = True
         _parse_gs(subparsers)
-        _parse_gen(subparsers)
+        _parse_kb(subparsers)
         return parser.parse_args()
     except Exception:
         parser.print_help()
@@ -153,14 +152,99 @@ def process_gs(args):
     print('\n'.join(ctn))
 
 
-def process_gen(args):
-    print(f'Process gen with simple:{args.simple}')
+def _validate_kb_cmd(cmd_keys):
+    if len(cmd_keys) > 2:
+        print(f'Commands too much')
+        exit(1)
+    if len(cmd_keys) == 2:
+        if 'edit' not in cmd_keys:
+            print(f'Multiple cmd keys: {" ".join(cmd_keys)}')
+            exit(1)
+        if 'edit' in cmd_keys and ['pods', 'exec'] in cmd_keys:
+            another_cmd = [x for x in cmd_keys if x != 'edit'][0]
+            print(f'Cannot edit {another_cmd}')
+            exit(1)
+        if len(set(cmd_keys)) == 1:
+            print(f'Duplicate command: {cmd_keys[0]}')
+            exit(1)
+    if len(cmd_keys) == 1 and 'edit' == cmd_keys[0]:
+        print('Cannot edit without another command')
+        exit(1)
+    if len(cmd_keys) == 0:
+        print(f'No kubectl command. Choice: {{{" ".join(KB_COMMANDS)}}}')
+        exit(1)
+
+
+def _process_kb_get(cmd, another_keys):
+    if len(another_keys) == 0:
+        print('No service found')
+        exit(1)
+    service = another_keys[0]
+    out = _cmd(f'kubectl -n {service} get {cmd}')
+    print(out)
+
+
+def _extract_service_pod(another_keys):
+    if len(another_keys) > 2:
+        print(f'Multiple kubectl arguments: {" ".join(another_keys)}')
+        exit(1)
+    service = ''
+    pod = ''
+    k1, k2 = another_keys
+    if k1.count('-') > k2.count('-'):
+        pod = k1
+        service = k2
+    else:
+        pod = k2
+        service = k1
+    return service, pod
+
+
+def _process_kb_edit(cmd, another_keys):
+    service, pod = _extract_service_pod(another_keys)
+    os.system(
+        f'KUBE_EDITOR=nano kubectl -n {service} edit {cmd} {pod}'
+    )
+
+
+def _process_kb_exec(another_keys):
+    service, pod = _extract_service_pod(another_keys)
+    os.system(
+        f'kubectl -n {service} exec -it {pod} -- sh'
+    )
+
+
+def _process_kb_cmd(cmd_keys, another_keys):
+    if len(cmd_keys) == 1:
+        cmd = cmd_keys[0]
+        if cmd == 'exec':
+            _process_kb_exec(another_keys)
+            return
+        if len(another_keys) > 1:
+            print(f'Multiple service detected: {" ".join(another_keys)}')
+            exit(1)
+        _process_kb_get(cmd, another_keys)
+    else:
+        cmd = [x for x in cmd_keys if x != 'edit'][0]
+        _process_kb_edit(cmd, another_keys)
+
+
+def process_kb(args):
+    cmd_keys = []
+    another_keys = []
+    for key in args.keywords:
+        if key in KB_COMMANDS:
+            cmd_keys.append(key)
+        else:
+            another_keys.append(key)
+    _validate_kb_cmd(cmd_keys)
+    _process_kb_cmd(cmd_keys, another_keys)
 
 
 def process(args):
     cmd_map = {
         'gs': process_gs,
-        'gen': process_gen
+        'kb': process_kb
     }
     cmd_map[args.cmd](args)
 
